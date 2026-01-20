eaa1e1b971fe43e0a94cc279916348ef97c917c6
Nguyễn Quang Hưng <148753898+nqhung5679@users.noreply.github.com>
2026-01-20 15:06:58 +0700

    Update ShowLog

--- Assets/ShowLog.cs
+++ Assets/ShowLog.cs
@@ -7,4 +7,10 @@ public class ShowLog : MonoBehaviour
     {
         Debug.Log("Hello World!");
     }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Update called! " + Time.frameCount);
+    }
 }
