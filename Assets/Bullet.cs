ad385a715f0a02a777c42943cb0d788ab81fb60b
Nguyễn Quang Hưng <148753898+nqhung5679@users.noreply.github.com>
2026-01-25 23:10:34 +0700

    feat: implement player shooting and bullet movement for Demo Part 2

---
+++ Assets/Bullet.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour
+{
+
+    public float flySpeed; // Biến tốc độ bay
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Lấy vị trí hiện tại
+        var newPosition = transform.position;
+
+        // Cộng thêm khoảng di chuyển theo trục Y
+        newPosition.y += Time.deltaTime * flySpeed;
+
+        // Cập nhật lại vị trí mới cho viên đạn
+        transform.position = newPosition;
+    }
+}
---
+++ Assets/PlayerShooting.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class PlayerShooting : MonoBehaviour
+{
+    // Các biến bắt buộc để không lỗi file Bonus.cs
+    public static PlayerShooting instance;
+    public int weaponPower = 1;
+    public int maxweaponPower = 5;
+
+    public GameObject bulletPrefabs;
+    public float shootingInterval = 0.2f;
+    private float lastBulletTime;
+
+    void Awake()
+    {
+        instance = this;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0)) // Giữ chuột trái
+        {
+            if (Time.time - lastBulletTime > shootingInterval)
+            {
+                Shoot();
+                lastBulletTime = Time.time;
+            }
+        }
+    }
+
+    void Shoot()
+    {
+        if (bulletPrefabs != null)
+        {
+            Instantiate(bulletPrefabs, transform.position, transform.rotation);
+        }
+    }
+}
